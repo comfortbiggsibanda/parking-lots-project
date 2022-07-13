f7bc7c706ac88ab30f7b39d08b9f68a12d452e85
comfortbiggsibanda <comfortbsibanda@gmail.com>
2022-07-13 13:31:53 +0200

    home page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const businessDataRouter = require('./routes/businessDataRoute');
+const viewRouter = require('./routes/viewRoute');
 
 
 // Start express app
@@ -13,7 +14,7 @@ app.use(bodyParser.json());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'csvData')));
 
-
+app.use('/', viewRouter);
 app.use('/api/v1/business-data', businessDataRouter);
 
 module.exports = app;
--- routes/businessDataRoute.js
+++ routes/businessDataRoute.js
@@ -3,10 +3,6 @@ const BusinessDataController = require('../controllers/businessDataController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hi there! Feel free to mess around with some of the routes requested for this assessment.')
-})
-
 router.post('/upload', BusinessDataController.uploadCsvFile, BusinessDataController.saveToDataBase);
 
 router.get('/parkinglots/:id/cars/:T', BusinessDataController.getAllCarsOnParkinglot)
